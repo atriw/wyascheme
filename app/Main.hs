4418cbde46936ffd70a86f94a0dae5bef4dad591
atriw <875241499@qq.com>
2022-05-04 01:26:12 +0800

    REPL

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,28 @@
 module Main where
 
-import Control.Monad (unless)
+import Control.Monad (unless, (>=>))
 import Lisp.Eval
 import Lisp.Parse
 import Lisp.Types
+import System.IO (hFlush, stdout)
 
 main :: IO ()
 main = run
 
 run :: IO ()
 run = do
-  putStrLn "input:"
-  input <- getLine
+  input <- readPrompt "Lisp>>> "
   unless (input == "quit")
-    (putStrLn (extractVal $ trapError $ show <$> (readExpr input >>= eval)) >> run)
+    (evalAndPrint input >> run)
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractVal (trapError $ show <$> (readExpr expr >>= eval))
+
+evalAndPrint :: String -> IO ()
+evalAndPrint = evalString >=> putStrLn
